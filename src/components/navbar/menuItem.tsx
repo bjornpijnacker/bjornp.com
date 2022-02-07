86acbc129b3f49de2cacb5d4e4be068ea1850a5a
Bjorn Pijnacker <bjorn.pijnacker@gmail.com>
2022-02-07 13:10:52 +0100

    added shadow to active menu item

--- src/components/navbar/menuItem.tsx
+++ src/components/navbar/menuItem.tsx
@@ -10,7 +10,7 @@ interface MenuItemProps {
 export default function MenuItem({ name, href, active }: MenuItemProps) {
     return (
         <Link to={href}
-              className={"px-3 py-2 rounded-lg text-sm font-medium" + (active ? " hover:bg-indigo-500 bg-indigo-500 text-white" : " hover:bg-indigo-100")}>
+              className={"px-3 py-2 rounded-lg text-sm font-medium" + (active ? " shadow hover:bg-indigo-500 bg-indigo-500 text-white" : " hover:bg-indigo-100")}>
             {name}</Link>
     );
 }
